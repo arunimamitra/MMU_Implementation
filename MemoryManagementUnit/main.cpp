6a3e4f92970feac477d3705b94d8cf618ed62074
Arunima Mitra <117327289+arunimamitra@users.noreply.github.com>
2023-03-31 18:36:42 -0400

    Update main.cpp

--- MemoryManagementUnit/main.cpp
+++ MemoryManagementUnit/main.cpp
@@ -2,14 +2,52 @@
 //  main.cpp
 //  MemoryManagementUnit
 //
-//  Created by Asmita Mitra on 3/30/23.
+//  Created by Arunima Mitra on 3/30/23.
 //  Copyright © 2023 Arunima. All rights reserved.
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+std::vector<int> randomValues;
+void createRandomArray(char* fileName){
+    ifstream rfile;
+    rfile.open(fileName);
+    //commit
+    if (rfile.is_open()==false) {
+        cout << "The gates of hell will open if I continue to parse with a non-existent file <%s>. Hence I decide to exit this program at this point avoiding insufferable pain\n" << fileName;
+        exit(1);
+    }
+    int elem;
+    while(rfile >> elem)
+        randomValues.push_back(elem);
+    randomValues.erase(randomValues.begin()); //erase first element which just contains size of the file
+    //cout<<randomValues.size()<<endl;
+    
+}
+
+class Process{
+    int pid;
+    int vmaCount;
+    int vma[][4];
+public:
+    Process(int a, int b, int c[][4]){
+        pid=a;
+        vmaCount=b;
+        for(int i=0;i<vmaCount;i++){
+            for(int j=0;j<4;j++){
+                vma[i][j] = c[i][j];
+            }
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    char* rfile= "/Users/asmitamitra/Desktop/Spring2023/OS/Lab2/lab2_assign/rfile";
+    createRandomArray(rfile);
     return 0;
 }
