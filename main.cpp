28259a01d752a2674496c9a6a2218379ac0e4eff
Atom-Agenda <117327289+Atom-Agenda@users.noreply.github.com>
2023-03-31 19:15:54 -0400

    initial commit

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 using namespace std;
 
 std::vector<int> randomValues;
+int ofs=1;
+ifstream file;
+
 void createRandomArray(char* fileName){
     ifstream rfile;
     rfile.open(fileName);
@@ -26,25 +29,52 @@ void createRandomArray(char* fileName){
         randomValues.push_back(elem);
     randomValues.erase(randomValues.begin()); //erase first element which just contains size of the file
     //cout<<randomValues.size()<<endl;
-    
 }
+int myrandom(int burst) { return  randomValues.at(ofs++%(randomValues.size()-1)) ; }
+
+class vma{
+    int beginVPage, endVPage, writeProtected, fileMapped;
+public:
+    void setValues(int b, int e, int w, int f){
+        beginVPage=b;
+        endVPage=e;
+        writeProtected=w;
+        fileMapped=f;
+    }
+    int getBeginPage(){ return beginVPage; }
+    int getEndPage(){ return beginVPage; }
+    int isWriteProtected(){ return writeProtected; }
+    int isFileMapped(){ return fileMapped; }
+    
+    void setBeginVPage(int b){beginVPage=b;}
+    void setEndVPage(int e){endVPage=e;}
+    void setWriteProtected(int w){ writeProtected=w; }
+    void setFileMapped(int f){ fileMapped=f;}
+};
 
 class Process{
     int pid;
     int vmaCount;
     int vma[][4];
 public:
-    Process(int a, int b, int c[][4]){
+    Process(int a, int b){
         pid=a;
         vmaCount=b;
-        for(int i=0;i<vmaCount;i++){
-            for(int j=0;j<4;j++){
-                vma[i][j] = c[i][j];
-            }
-        }
+        
     }
 };
 
+
+void initialize(char* fileName){
+    file.open(fileName);
+    string lineNow;
+     
+     if (file.is_open()==false) {
+         cout << "The gates of hell will open if I continue to parse with a non-existent file <%s>. Hence I decide to exit this program at this point avoiding insufferable pain\n" << fileName;
+         exit(1);
+     }
+    
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     char* rfile= "/Users/asmitamitra/Desktop/Spring2023/OS/Lab2/lab2_assign/rfile";
